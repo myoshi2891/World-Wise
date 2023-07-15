37483dea86680bcccd72fa30d2adb87aeb2f7b3e
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-07-16 07:08:40 +0900

    Using CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
-				<Route path="pricing" element={<Pricing />} />
 				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="app" element={<AppLayout />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
 
 function Homepage() {
 	return (
 		<div>
 			<PageNav />
-			<h1>World Wise</h1>
-			<Link to="/pricing">Pricing</Link>
+			<AppNav />
+			<h1 className="test">World Wise</h1>
+			<Link to="/app">Go to the app</Link>
 		</div>
 	);
 }
