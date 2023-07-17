34553bdd585d55b13b137067abc86d6bc80d7a97
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-07-18 02:58:53 +0900

    Fetching City Data in the Form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,15 +14,20 @@ import styles from "./Map.module.css";
 import { useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useEffect } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
 	const { cities } = useCities();
-
 	const [mapPosition, setMapPosition] = useState([40, 0]);
+	const {
+		isLoading: isLoadingPosition,
+		position: geolocationPosition,
+		getPosition,
+	} = useGeolocation();
 
-	const [searchParams] = useSearchParams();
-	const mapLat = searchParams.get("lat");
-	const mapLng = searchParams.get("lng");
+	const [mapLat, mapLng] = useUrlPosition();
 
 	useEffect(
 		function () {
@@ -31,8 +36,24 @@ function Map() {
 		[mapLat, mapLng]
 	);
 
+	useEffect(
+		function () {
+			if (geolocationPosition)
+				setMapPosition([
+					geolocationPosition.lat,
+					geolocationPosition.lng,
+				]);
+		},
+		[geolocationPosition]
+	);
+
 	return (
 		<div className={styles.mapContainer}>
+			{!geolocationPosition && (
+				<Button type="position" onClick={getPosition}>
+					{isLoadingPosition ? "Loading..." : "Use your position"}
+				</Button>
+			)}
 			<MapContainer
 				center={mapPosition}
 				zoom={6}
