be92537c2a90f13ce0c60a5d5dbfa94c116e2343
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-07-17 18:08:07 +0900

    Finishing the City View

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,5 +1,4 @@
-import { useContext } from "react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 
 const CitiesContext = createContext();
 
@@ -8,6 +7,7 @@ const BASE_URL = "http://localhost:8000";
 function CitiesProvider({ children }) {
 	const [cities, setCities] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [currentCity, setCurrentCity] = useState({});
 
 	useEffect(function () {
 		async function fetchCities() {
@@ -25,11 +25,26 @@ function CitiesProvider({ children }) {
 		fetchCities();
 	}, []);
 
+	async function getCity(id) {
+		try {
+			setIsLoading(true);
+			const res = await fetch(`${BASE_URL}/cities/${id}`);
+			const data = await res.json();
+			setCurrentCity(data);
+		} catch {
+			alert("There was an error loading data...");
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
 	return (
 		<CitiesContext.Provider
 			value={{
 				cities,
 				isLoading,
+				currentCity,
+				getCity,
 			}}
 		>
 			{children}
